cf461613fdf7c3d3cf04127904a1627bd0bcc62e
unknown <hadziqnaufal71@gmail.com>
2024-12-01 20:38:45 +0700

    membuat menu data

--- data.py
+++ data.py
@@ -1,24 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-# Title of the app
 st.title("Explorasi Dataset")
 
-# Load the dataset from a local file
-file_path = "Food_and_Nutrition__.csv"  # Pastikan file ini berada di direktori yang sama dengan script
+file_path = "Food_and_Nutrition__.csv"  
 try:
     data = pd.read_csv(file_path)
     
-    # Display the dataset
     st.subheader("Dataset yang Dimuat:")
     st.write(data)
     
-    # Show basic information about the dataset
     st.subheader("Informasi Dataset:")
     st.write(f"Jumlah Baris dan Kolom: {data.shape}")
     st.write(f"Kolom dalam Dataset: {data.columns.tolist()}")
     
-    # Show descriptive statistics
     st.subheader("Statistik Deskriptif:")
     st.write(data.describe(include="all"))
 
